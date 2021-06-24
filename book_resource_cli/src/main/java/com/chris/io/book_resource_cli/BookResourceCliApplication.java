404f874afa8720f9cc73664bcd2b6b82d7c30572
chengkejun <chengkejun@xinye.com>
2021-06-24 13:34:01 +0800

    update sidebar & init cli

---
+++ book_resource_cli/src/main/java/com/chris/io/book_resource_cli/BookResourceCliApplication.java
@@ -0,0 +1,13 @@
+package com.chris.io.book_resource_cli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookResourceCliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookResourceCliApplication.class, args);
+    }
+
+}
---
+++ book_resource_cli/src/test/java/com/chris/io/book_resource_cli/BookResourceCliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chris.io.book_resource_cli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookResourceCliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
